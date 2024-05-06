3c750447709c1b1e751e58b0609eaf9cd7af010f
Najmus Sahar <najmus.sahar77@gmail.com>
2024-05-06 21:14:48 +0530

    initial commit2

--- Services/obj/Debug/netcoreapp3.1/Services.AssemblyInfo.cs
+++ Services/obj/Debug/netcoreapp3.1/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6785a75726376c5cbbfe96f2f43207dc6bb014a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
